51fa0f659bf417bfa1ea90c8b0fbb820a44b2e23
Michael Palmos <toqoz@hotmail.com>
2018-06-29 14:18:52 +1000

    Create SaveManager.cs

---
+++ inventory/SaveManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SaveManager {
+    public static void LoadOrInitializeInventory(Inventory inventoryTemplate) {
+        // Saving and loading.
+        if (File.Exists(Path.Combine(Application.persistentDataPath, "inventory.json"))) {
+            Debug.Log("Found file inventory.json, loading inventory.");
+            Inventory.LoadFromJSON(Path.Combine(Application.persistentDataPath, "inventory.json"));
+        } else {
+            Debug.Log("Couldn't find inventory.json, loading from template.");
+            Inventory.InitializeFromDefault(inventoryTemplate);
+        }
+    }
+    public static void SaveInventory() {
+        Inventory.Instance.SaveToJSON(Path.Combine(Application.persistentDataPath, "inventory.json"));
+    }
+
+    // Load from default.
+    public static void LoadFromTemplate(Inventory template) {
+        Inventory.InitializeFromDefault(template);
+    }
+}
