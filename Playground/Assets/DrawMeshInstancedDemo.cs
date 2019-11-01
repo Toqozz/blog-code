f9f3a006c940ab05307fd9710a6c9d236fd6ff9f
Michael Palmos <toqoz@hotmail.com>
2019-11-01 18:37:51 +1000

    mesh batching post

---
+++ Playground/Assets/DrawMeshInstancedDemo.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawMeshInstancedDemo : MonoBehaviour {
+    // How many meshes to draw.
+    public int population;
+    // Range to draw meshes within.
+    public float range;
+
+    // Material to use for drawing the meshes.
+    public Material material;
+  
+    // CPU.
+    private Matrix4x4[] matrices;
+    private MaterialPropertyBlock block;
+
+    private Mesh mesh;
+
+    private void Setup() {
+        Mesh mesh = CreateQuad();
+        this.mesh = mesh;
+
+        matrices = new Matrix4x4[population];
+        Vector4[] colors = new Vector4[population];
+
+        block = new MaterialPropertyBlock();
+
+        for (int i = 0; i < population; i++) {
+            // Build matrix.
+            Matrix4x4 mat = Matrix4x4.identity;
+            Vector3 position = new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
+            Quaternion rotation = Quaternion.Euler(Random.Range(-180, 180), Random.Range(-180, 180), Random.Range(-180, 180));
+            Vector3 scale = Vector3.one;
+
+            mat.SetTRS(position, rotation, scale);
+
+            matrices[i] = mat;
+
+            colors[i] = Color.Lerp(Color.red, Color.blue, Random.value);
+        }
+
+        block.SetVectorArray("_Colors", colors);
+    }
+
+    private Mesh CreateQuad(float width = 1f, float height = 1f) {
+        // Create a quad mesh.
+        var mesh = new Mesh();
+
+        float w = width * .5f;
+        float h = height * .5f;
+        var vertices = new Vector3[4] {
+            new Vector3(-w, -h, 0),
+            new Vector3(w, -h, 0),
+            new Vector3(-w, h, 0),
+            new Vector3(w, h, 0)
+        };
+
+        var tris = new int[6] {
+            // lower left tri.
+            0, 2, 1,
+            // lower right tri
+            2, 3, 1
+        };
+
+        var normals = new Vector3[4] {
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+        };
+
+        var uv = new Vector2[4] {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1),
+        };
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        return mesh;
+    }
+
+    private void Start() {
+        Setup();
+    }
+
+    private void Update() {
+        Graphics.DrawMeshInstanced(mesh, 0, material, matrices, population, block);
+    }
+}
---
+++ Playground/Assets/InstancedColor.shader
@@ -0,0 +1,51 @@
+﻿Shader "Custom/InstancedColor" {
+    SubShader {
+        Tags { "RenderType" = "Opaque" }
+
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_instancing
+            
+            #include "UnityCG.cginc"
+            
+            struct appdata_t {
+                float4 vertex   : POSITION;
+                float4 color    : COLOR;
+                float2 texcoord : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f {
+                float4 vertex   : SV_POSITION;
+                fixed4 color    : COLOR;
+                float2 texcoord : TEXCOORD0;
+            }; 
+
+            float4 _Colors[1023];
+
+            v2f vert(appdata_t i, uint instanceID: SV_InstanceID) {
+                // Allow instancing.
+                UNITY_SETUP_INSTANCE_ID(i);
+
+                v2f o;
+                o.vertex = UnityObjectToClipPos(i.vertex);
+                o.color = float4(1, 1, 1, 1);
+
+                // If instancing on (it should be) assign per-instance color.
+                #ifdef UNITY_INSTANCING_ENABLED
+                    o.color = _Colors[instanceID];
+                #endif
+                
+                return o;
+            }
+            
+            fixed4 frag(v2f i) : SV_Target {
+                return i.color;
+            }
+            
+            ENDCG
+        }
+    }
+}
